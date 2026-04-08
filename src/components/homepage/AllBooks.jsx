e7bf4961f2c5c0932827d8b310161dc1e11726ce
Nafiz Alam <nafizalam.dev@gmail.com>
2026-04-09 02:51:30 +0600

    Book Page Complete

--- src/components/homepage/AllBooks.jsx
+++ src/components/homepage/AllBooks.jsx
@@ -1,5 +1,5 @@
 import React, { use } from 'react';
-import { FaRegStar } from 'react-icons/fa';
+import BookCard from '../ui/BookCard';
 
 const booksPromise = fetch("/booksData.json").then((res) => res.json());
 
@@ -8,40 +8,16 @@ const AllBooks = () => {
     const books = use(booksPromise);
 
     return (
-        <div className='container mx-auto mt-24 flex flex-col gap-9'>
+        <div className='container mx-auto mt-24 flex flex-col gap-9 text-center'>
             <h1 className='text-[40px] font-bold'>Book</h1>
 
-            {
-                books.map((book) => {
-                    return (
-                        <div className="card bg-base-100 w-96 shadow-sm p-6 border border-[#131313]/15 rounded-2xl">
-                            <figure>
-                                <img
-                                    src={book.image}
-                                    alt={book.bookName} />
-                            </figure>
-                            <div className="card-body p-0 flex flex-col gap-4 pb-2.5 border-b border-dashed border-[#131313]/15 mt-6">
-                                <div className="flex gap-3">
-                                    {book.tags.map((tag) => (
-                                        <div className="badge badge-soft badge-success">{tag}</div>
-                                    ))}
-                                </div>
-                                <h2 className="card-title text-2xl font-bold">
-                                    {book.bookName}
-                                </h2>
-                                <p className='font-medium'>By: {book.author}</p>
-                            </div>
-                            <div className="flex items-center justify-between mt-2.5">
-                                <p className='font-medium'>{book.category}</p>
-                                <div className="flex items-center gap-1">
-                                    <p className='font-medium'>{book.rating}</p>
-                                    <FaRegStar />
-                                </div>
-                            </div>
-                        </div>
-                    )
-                })
-            }
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
+                {
+                    books.map((book, ind) => {
+                            return  <BookCard key={ind} book={book} /> 
+                    })
+                }
+            </div>
         </div>
     );
 };
